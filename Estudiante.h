28f150e01bfc064f37d4b095bfeee20fb8bcfeb2
IkerFeda <iferndac@myuax.com>
2023-12-14 12:13:07 +0100

    .

--- Estudiante.h
+++ Estudiante.h
@@ -1,20 +1,20 @@
 #ifndef IKER_FERNANDEZ_EXAMEN2CPP_ESTUDIANTE_H
 #define IKER_FERNANDEZ_EXAMEN2CPP_ESTUDIANTE_H
 
-#include "iostream"
-#include "string"
-
+#include <iostream>
+#include <string>
 
 class Estudiante {
-    public:
+public:
     std::string nombre;
     int edad;
     float nota;
 
-    void MostrarDatos();
-    std::cout << "Nombre: " << Estudiante.nombre << std::endl;
-    std::cout << "Edad: " << Estudiante.edad << std::endl;
-    std::cout << "Promedio: " << Estudiante.nota << std::endl;
+    void MostrarDatos() {
+        std::cout << "Nombre: " << nombre << std::endl;
+        std::cout << "Edad: " << edad << std::endl;
+        std::cout << "Promedio: " << nota << std::endl;
+    }
 };
 
 
