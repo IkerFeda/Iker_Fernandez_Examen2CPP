b1ae5c09e085dc079f993aab565e0de084849d2c
IkerFeda <iferndac@myuax.com>
2023-12-14 12:17:19 +0100

    .

--- MostrarEstudiante.c
+++ MostrarEstudiante.c
@@ -1,5 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 struct Estudiante {
     char nombre[50];
@@ -11,4 +12,15 @@ void MostrarDatos(struct Estudiante estudiante) {
     printf("Nombre: %s\n", estudiante.nombre);
     printf("Edad: %d\n", estudiante.edad);
     printf("Promedio: %f\n", estudiante.nota);
+}
+
+int main(){
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre, "Iker");
+    estudiante1.edad = 20;
+    estudiante1.nota = 9.5;
+
+    MostrarDatos(estudiante1);
+
+    return 0;
 }
