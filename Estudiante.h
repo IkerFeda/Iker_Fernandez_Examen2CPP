f52b2ff05ff43885978d02fa22f9c9977d8da3c3
IkerFeda <iferndac@myuax.com>
2023-12-14 13:16:31 +0100

    .

--- Estudiante.h
+++ Estudiante.h
@@ -1,19 +1,26 @@
-#ifndef IKER_FERNANDEZ_EXAMEN2CPP_ESTUDIANTE_H
-#define IKER_FERNANDEZ_EXAMEN2CPP_ESTUDIANTE_H
+#ifndef ESTUDIANTE_H
+#define ESTUDIANTE_H
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 // Definición de la estructura Estudiante
 struct Estudiante {
     std::string nombre;
     int edad;
     float promedio;
+    std::vector<std::string> materias; // Vector de materias
+
+    // Función para agregar materia
+    void agregarMateria(const std::string& materia);
+
+    // Función para eliminar materia
+    void eliminarMateria(const std::string& materia);
+
+    // Función para mostrar las materias del estudiante
+    void mostrarMaterias();
 };
 
-// Declaración de la función mostrarEstudiante
-void mostrarEstudiante(const Estudiante& estudiante);
+#endif  // ESTUDIANTE_H
 
-
-
-#endif //IKER_FERNANDEZ_EXAMEN2CPP_ESTUDIANTE_H
