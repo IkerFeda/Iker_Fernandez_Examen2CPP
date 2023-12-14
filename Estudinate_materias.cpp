d4e9834f977adf1cf480e6f446ef2ad1614a4c1a
IkerFeda <iferndac@myuax.com>
2023-12-14 13:16:47 +0100

    .

--- Estudinate_materias.cpp
+++ Estudinate_materias.cpp
@@ -1,3 +1,26 @@
-//
-// Created by ikerf on 14/12/2023.
-//
+#include "Estudiante.h"
+
+int main() {
+    // Crear un estudiante
+    Estudiante estudiante1;
+    estudiante1.nombre = "Juan";
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+
+    // Agregar materias
+    estudiante1.agregarMateria("Matemáticas");
+    estudiante1.agregarMateria("Física");
+    estudiante1.agregarMateria("Programación");
+
+    // Mostrar las materias del estudiante
+    estudiante1.mostrarMaterias();
+
+    // Eliminar una materia
+    estudiante1.eliminarMateria("Física");
+
+    // Mostrar las materias actualizadas del estudiante
+    estudiante1.mostrarMaterias();
+
+    return 0;
+}
+
