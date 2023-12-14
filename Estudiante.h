27e425fbfb72da63967e371c6f6e8ea34626a5c8
IkerFeda <iferndac@myuax.com>
2023-12-14 11:56:53 +0100

    .

--- Estudiante.h
+++ Estudiante.h
@@ -1,8 +1,17 @@
-//
-// Created by ikerf on 14/12/2023.
-//
-
 #ifndef IKER_FERNANDEZ_EXAMEN2CPP_ESTUDIANTE_H
 #define IKER_FERNANDEZ_EXAMEN2CPP_ESTUDIANTE_H
+
+#include "iostream"
+#include "string"
+
+
+class Estudiante {
+    public:
+    std::string nombre;
+    int edad;
+    float nota;
+
+};
+
 
 #endif //IKER_FERNANDEZ_EXAMEN2CPP_ESTUDIANTE_H
